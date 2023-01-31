b1cd82bee172bfc6fe54651722cafbaed1b5967d
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-01-31 15:27:43 -0700

    finalized initialization of classes

--- rpg.py
+++ rpg.py
@@ -1,19 +1,19 @@
 class Weapon:
-    def __init__(self, name, attack)
+    def __init__(self, name, attack):
         self._name = name
         self._attack = attack
-    def get_weapon_name(self):
+    def get_name(self):
         return self._name
-    def get_weapon_attack(self):
+    def get_attack(self):
         return self._attack
 
 class Armor:
-    def __init__(self, name, defense)
+    def __init__(self, name, defense):
         self._name = name
         self._defense = defense
-    def get_weapon_name(self):
+    def get_name(self):
         return self._name
-    def get_weapon_attack(self):
+    def get_defense(self):
         return self._attack
 
 class Item:
@@ -38,7 +38,7 @@ bread = Item("Bread", 10)
 
 
 class Tank:
-    def __init__(self, name, weapon={}, armor={})
+    def __init__(self, name, weapon={}, armor={}):
         self._name = name
         self._health = 100
         self._attack = 10
@@ -47,20 +47,25 @@ class Tank:
         self._armor = armor
         self._inventory = {}
         self._deceased = False
-        
     def get_health(self):
         return self._health
     def get_weapon(self):
         return self._weapon
     def set_health(self, new_health):
         self._health = new_health
-    def set_weapon(self, old_weapon, new_weapon):
+    def set_weapon(self, new_weapon):
+        self._attack -= self.get_weapon().get_attack()
         self._weapon = new_weapon
-        self._attack -= old_weapon.get_weapon_attack()
-        self._attack += new_weapon.get_weapon_attack()
+        self._attack += new_weapon.get_attack()
+    def get_armor(self):
+        return self._armor
+    def set_armor(self, new_armor):
+        self._defense -= self._armor.get_defense()
+        self._armor = new_armor
+        self._defense += new_armor.get_defense()
 
 class Engineer:
-    def __init__(self, name, weapon={}, armor={})
+    def __init__(self, name, weapon={}, armor={}):
         self._name = name
         self._health = 100
         self._attack = 1
@@ -77,11 +82,17 @@ class Engineer:
         self._health = new_health
     def set_weapon(self, old_weapon, new_weapon):
         self._weapon = new_weapon
-        self._attack -= old_weapon.get_weapon_attack()
-        self._attack += new_weapon.get_weapon_attack()
+        self._attack -= old_weapon.get_attack()
+        self._attack += new_weapon.get_attack()
+    def get_armor(self):
+        return self._armor
+    def set_armor(self, new_armor):
+        self._defense -= self._armor.get_defense()
+        self._armor = new_armor
+        self._defense += new_armor.get_defense()
 
 class Archer:
-    def __init__(self, name, weapon={}, armor={})
+    def __init__(self, name, weapon={}, armor={}):
         self._name = name
         self._health = 100
         self._attack = 7
@@ -98,8 +109,14 @@ class Archer:
         self._health = new_health
     def set_weapon(self, old_weapon, new_weapon):
         self._weapon = new_weapon
-        self._attack -= old_weapon.get_weapon_attack()
-        self._attack += new_weapon.get_weapon_attack()
+        self._attack -= old_weapon.get_attack()
+        self._attack += new_weapon.get_attack()
+    def get_armor(self):
+        return self._armor
+    def set_armor(self, new_armor):
+        self._defense -= self._armor.get_defense()
+        self._armor = new_armor
+        self._defense += new_armor.get_defense()
 
 #squad list
 isaac = Tank("Isaac", club, lightmail)
@@ -109,12 +126,10 @@ meghan = Archer("Meghan", magic_wand, chainmail)
 will = Tank("Will", sword, lightmail)
 
 class Team:
-
-    def __init__(self, slot1, slot2={}, slot3={})
+    def __init__(self, slot1, slot2={}, slot3={}):
         self.slot1 = slot1
         self.slot2 = slot2
         self.slot3 = slot3
-
     def set_squad(self, slot, new_character):
         if slot == "1":
             self.slot1 = new_character
@@ -122,8 +137,20 @@ class Team:
             self.slot2 = new_character
         if slot == "3":
             self.slot3 = new_character
+    def get_squad_slot1(self):
+        return self.slot1
+    def get_squad_slot2(self):
+        return self.slot2 
+    def get_squad_slot3(self):
+        return self.slot3 
+
 
 #Teams
 team1 = Team(isaac, emilee, aiden)
 team2 = Team(will, meghan, isaac)
-team3 = Team(aiden, emilee, meghan)
+team3 = Team(aiden, emilee, will)
+
+print(team1.get_squad_slot1().get_weapon().get_name())
+print(meghan.get_weapon().get_name())
+print(team2.get_squad_slot2().get_armor().get_name())
+print(team3.get_squad_slot3().get_weapon().get_attack())
