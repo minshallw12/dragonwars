45af92edbfff0a087005ea0cea19d033d28e29e6
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-01-31 15:11:29 -0700

    added items, and initialized examples

--- rpg.py
+++ rpg.py
@@ -1,98 +1,112 @@
 class Weapon:
     def __init__(self, name, attack)
-        self.name = name
-        self.attack = attack
+        self._name = name
+        self._attack = attack
     def get_weapon_name(self):
-        return self.name
+        return self._name
     def get_weapon_attack(self):
-        return self.attack
+        return self._attack
 
 class Armor:
     def __init__(self, name, defense)
-        self.name = name
-        self.defense = defense
+        self._name = name
+        self._defense = defense
     def get_weapon_name(self):
-        return self.name
+        return self._name
     def get_weapon_attack(self):
-        return self.attack
+        return self._attack
+
+class Item:
+    def __init__(self, name, value):
+        self._name = name
+        self._value = value
+    def get_item_value(self):
+        return self._value
 
 #weapons list
 club = Weapon("Club", 10)
-sword = Weapon("Crossbow", 5)
+sword = Weapon("Sword", 5)
 magic_wand = Weapon("Magic Wand", 15)
 #armor list
 chainmail = Armor("Chainmail", 10)
 lightmail = Armor("Lightmail", 3)
 dragonmail = Armor("Dragonmail", 25)
+#item list
+health_potion = Item("Health Potion", 40)
+grenade = Item("Grenade", 20)
+bread = Item("Bread", 10)
 
 
 class Tank:
     def __init__(self, name, weapon={}, armor={})
-        self.name = name
-        self.health = 100
-        self.attack = 5
-        self.defense = 15
-        self.weapon = weapon
-        self.armor = armor
-        self.deceased = False
+        self._name = name
+        self._health = 100
+        self._attack = 10
+        self._defense = 15
+        self._weapon = weapon
+        self._armor = armor
+        self._inventory = {}
+        self._deceased = False
+        
     def get_health(self):
-        return self.health
+        return self._health
     def get_weapon(self):
-        return self.weapon
+        return self._weapon
     def set_health(self, new_health):
-        self.health = new_health
+        self._health = new_health
     def set_weapon(self, old_weapon, new_weapon):
-        self.weapon = new_weapon
-        self.attack -= old_weapon.get_weapon_attack()
-        self.attack += new_weapon.get_weapon_attack()
+        self._weapon = new_weapon
+        self._attack -= old_weapon.get_weapon_attack()
+        self._attack += new_weapon.get_weapon_attack()
 
 class Engineer:
     def __init__(self, name, weapon={}, armor={})
-        self.name = name
-        self.health = 100
-        self.attack = 1
-        self.defense = 50
-        self.weapon = weapon
-        self.armor = armor
-        self.deceased = False
+        self._name = name
+        self._health = 100
+        self._attack = 1
+        self._defense = 10
+        self._weapon = weapon
+        self._armor = armor
+        self._inventory = {}
+        self._deceased = False
     def get_health(self):
-        return self.health
+        return self._health
     def get_weapon(self):
-        return self.weapon
+        return self._weapon
     def set_health(self, new_health):
-        self.health = new_health
+        self._health = new_health
     def set_weapon(self, old_weapon, new_weapon):
-        self.weapon = new_weapon
-        self.attack -= old_weapon.get_weapon_attack()
-        self.attack += new_weapon.get_weapon_attack()
+        self._weapon = new_weapon
+        self._attack -= old_weapon.get_weapon_attack()
+        self._attack += new_weapon.get_weapon_attack()
 
 class Archer:
-    def __init__(self, name, weapon={}, amror={})
-        self.name = name
-        self.health = 100
-        self.attack = 15
-        self.defense = 5
-        self.weapon = weapon
-        self.armor = armor
-        self.deceased = False
+    def __init__(self, name, weapon={}, armor={})
+        self._name = name
+        self._health = 100
+        self._attack = 7
+        self._defense = 2
+        self._weapon = weapon
+        self._armor = armor
+        self._inventory = {}
+        self._deceased = False
     def get_health(self):
-        return self.health
+        return self._health
     def get_weapon(self):
-        return self.weapon
-    def get_armor(self):
-        return self.armor
+        return self._weapon
     def set_health(self, new_health):
-        self.health = new_health
+        self._health = new_health
     def set_weapon(self, old_weapon, new_weapon):
-        self.weapon = new_weapon
-        self.attack -= old_weapon.get_weapon_attack()
-        self.attack += new_weapon.get_weapon_attack()
-    def set_armor(self, old_armor, new_armor):
-        self.armor = new_armor
-        self.defense -= old_armor.get_armor_defense()
-        self.defense += new_armor.get_armor_defense()
+        self._weapon = new_weapon
+        self._attack -= old_weapon.get_weapon_attack()
+        self._attack += new_weapon.get_weapon_attack()
 
-
+#squad list
+isaac = Tank("Isaac", club, lightmail)
+emilee = Archer("Emilee", magic_wand, dragonmail)
+aiden = Engineer("Aiden", sword, chainmail)
+meghan = Archer("Meghan", magic_wand, chainmail)
+will = Tank("Will", sword, lightmail)
 
 class Team:
 
@@ -108,3 +122,8 @@ class Team:
             self.slot2 = new_character
         if slot == "3":
             self.slot3 = new_character
+
+#Teams
+team1 = Team(isaac, emilee, aiden)
+team2 = Team(will, meghan, isaac)
+team3 = Team(aiden, emilee, meghan)
